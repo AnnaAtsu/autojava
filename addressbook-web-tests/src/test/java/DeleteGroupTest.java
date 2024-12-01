d7dc42e96a3cc176235cd8495dd9624d770f54af
Andrey Sidorov <takandar@gmail.com>
2024-12-01 21:12:44 +0300

    Первая часть №6: Реализовать двухуровневую архитектуру тестового набора

--- addressbook-web-tests/src/test/java/DeleteGroupTest.java
+++ addressbook-web-tests/src/test/java/DeleteGroupTest.java
@@ -1,62 +1,17 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 
-import java.util.NoSuchElementException;
-
-public class DeleteGroupTest {
-    private static WebDriver driver;
-
-
-    @BeforeEach
-    public void setUp() {
-        if (driver == null) {
-            driver = new EdgeDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(1904, 1119));
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-    }
+public class DeleteGroupTest extends TestBase{
 
 
     @Test
     public void deleteGroup() {
         driver.findElement(By.linkText("groups")).click();
-        if(!isSelectPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
-        }
-        if(!isSelectPresent(By.name("selected[]"))) {
-            driver.findElement(By.name("new")).click();
-            driver.findElement(By.name("group_name")).click();
-            driver.findElement(By.name("group_name")).sendKeys("anna1");
-            driver.findElement(By.name("group_header")).click();
-            driver.findElement(By.name("group_header")).sendKeys("max");
-            driver.findElement(By.name("group_footer")).click();
-            driver.findElement(By.name("group_footer")).sendKeys("moore");
-            driver.findElement(By.name("submit")).click();
-            driver.findElement(By.linkText("group page")).click();
-        }
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.cssSelector(".group:nth-child(8) > input")).click();
-        driver.findElement(By.name("delete")).click();
-        driver.findElement(By.linkText("group page")).click();
+        openGroupPage();
+        isGroupPresent();
+        deleteGroupPage();
         driver.findElement(By.linkText("Logout")).click();
     }
 
 
-    private boolean isSelectPresent(By locator) {
-        try {
-            driver.findElement(locator);
-           return true;
-       }
-        catch (NoSuchElementException exception) {
-        return false;
-        }
-    }
 }
