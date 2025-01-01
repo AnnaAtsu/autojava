8d2564c19f450920f13d06eee0a3d80d52573ac1
Andrey Sidorov <takandar@gmail.com>
2025-01-01 16:08:01 +0300

    Задание №16 доработанное: Реализовать тест для проверки информации о контактах на главной странице

--- addressbook-web-tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactInfoTests.java
@@ -54,5 +54,31 @@ public class ContactInfoTests extends TestBase {
 
     }
 
+//Задание №16 в одном тесте: Реализовать тест для проверки информации о контактах на главной странице
 
+
+    @Test
+    void testContactInformation() {
+        if (app.hbmcontact().getContactCount() == 0) {
+            app.hbmcontact().createContactshort(new DataContact());
+        }
+        var contacts = app.hbmcontact().getContactList();
+        var contact = contacts.get(0);
+        var phones = app.allcontacts().getPhones(contact);
+        var emails = app.allcontacts().getEmails(contact);
+        var addresses = app.allcontacts().getAddresses(contact);
+        var expectedphones = Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
+                .filter(s -> s != null && !"".equals(s))
+                .collect(Collectors.joining("\n"));
+        var expectedemails = Stream.of(contact.email(), contact.email2(), contact.email3())
+                .filter(s -> s != null && !"".equals(s))
+                .collect(Collectors.joining("\n"));
+
+        var expectedaddreses = Stream.of(contact.address(), contact.address2())
+                .filter(s -> s != null && !"".equals(s))
+                .collect(Collectors.joining("\n"));
+        Assertions.assertEquals(expectedphones, phones);
+        Assertions.assertEquals(expectedemails, emails);
+        Assertions.assertEquals(expectedaddreses, addresses);
+    }
 }
