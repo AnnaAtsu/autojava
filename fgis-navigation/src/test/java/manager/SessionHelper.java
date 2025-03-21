8b48d5d0dc39147de54cc20922e6bb7c376ef9fb
Andrey Sidorov <takandar@gmail.com>
2025-03-21 09:19:27 +0300

    fgis-test1 добавлено

--- fgis-navigation/src/test/java/manager/SessionHelper.java
+++ fgis-navigation/src/test/java/manager/SessionHelper.java
@@ -1,23 +1,34 @@
 package manager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static manager.ApplicationManager.driver;
+import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 
-public class SessionHelper extends HelperBase{
+
+public class SessionHelper extends HelperBase {
 
     public SessionHelper(ApplicationManager manager) {
         super(manager);
     }
+
     public void login(String user, String password) {
 
-        click(By.id("input-57"));
-        type(By.id("input-57"),user);
-        click(By.id("input-67"));
-        type(By.id("input-67"), password);
+        click(By.id("input-58"));
+        type(By.id("input-58"), user);
+        click(By.id("input-68"));
+        type(By.id("input-68"), password);
         click(By.xpath("//span[contains(.,'Войти')]"));
     }
 
@@ -40,11 +51,11 @@ public class SessionHelper extends HelperBase{
     }
 
     public boolean checkTablePart() {
-        isElementPresent(By.tagName("tr"));
+        isElementPresent(By.cssSelector("span.text-start"));
         return true;
     }
 
-    public void  goToReestrOtborov() {
+    public void goToReestrOtborov() {
         WebElement OtborovButton = driver.findElement(By.xpath("//*[contains(text(), \" Реестр отборов проб \")]"));
         OtborovButton.click();
     }
@@ -78,11 +89,101 @@ public class SessionHelper extends HelperBase{
     public void generateNumber() {
         WebElement putOnDataButton = driver.findElement(By.xpath("//*[contains(text(), \"Внести данные\")]"));
         putOnDataButton.click();
-        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-       // WebElement generateDataButton = driver.findElement(By.xpath("//span[@class='btn__content' and text='Сгенерировать']"));
-        //WebElement generateDataButton = driver.findElement(By.xpath("//div[2]/div[2]/div/button[2]/span/span[2]"));
-        WebElement generateDataButton = driver.findElement(By.xpath("//*[@class='btn__content' and text='Сгенерировать']"));
-        generateDataButton.click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
+        //  {
+        // WebElement element = driver.findElement(By.tagName("body"));
+       // Actions builder = new Actions(driver);
+        // builder.moveToElement(element, 0, 0).perform();
+       //  }
+
+        driver.switchTo().activeElement();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //явное ожидание
+        //  WebElement button = (new WebDriverWait(driver, Duration.ofSeconds(8))
+        //       .until(ExpectedConditions.presenceOfElementLocated(By.xpath(("//*[@class='btn__content' and text='Сгенерировать']")))));
+        //неявное ожидание
+        //  button.click();
+     //Второй способ
+// Получаем идентификаторы всех окон
+    //    Set<String> windowHandles = driver.getWindowHandles();
+    //    String mainWindowHandle = driver.getWindowHandle();  // Основное окно
+
+        // Переключаемся на всплывающее окно
+    //    for (String handle : windowHandles) {
+     //       if (!handle.equals(mainWindowHandle)) {
+      //          driver.switchTo().window(handle);
+               // break;
+      //      }
+      //  }
+
+        // Находим кнопку "Сгенерировать" и нажимаем её
+        //WebElement generateButton = driver.findElement(By.xpath("//button[text()='Сгенерировать']"));
+        WebElement generateButton = driver.findElement(By.xpath("//*[contains(text(), \"Сгенерировать\")]"));
+        generateButton.click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+
+
+    public int getCount() {
+        return driver.findElements(By.cssSelector("span.text-start")).size();
+    }
+
+    public void goToReestrNomerovLots() {
+        WebElement reestrNomerovLOTButton = driver.findElement(By.xpath("//*[contains(text(), \" Реестр выданных номеров партий зерна \")]"));
+        reestrNomerovLOTButton.click();
+
+    }
+
+    public void generatelotNumber() {
+        WebElement putOnDataButton = driver.findElement(By.xpath("//*[contains(text(), \"Внести данные\")]"));
+        putOnDataButton.click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement chculture = driver.findElement(By.cssSelector("div.v-input--hide-details"));
+
+        //chculture.deSelectByContainsVisibleText("Арахис (орех земляной) лущеный ( ОКПД 2: 01.11.83 )");
+        chculture.click();
+        chculture.sendKeys("01.11.83.000", Keys.ENTER);
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        WebElement generateDataButton = driver.findElement(By.xpath("//*[contains(text(), \"Сгенерировать\")]"));
+        generateDataButton.click();
+    }
+
+    public void goToReestrNomerovLotsPPZ() {
+        WebElement reestrNomerovLOTPPZButton = driver.findElement(By.xpath("//*[contains(text(), \" Реестр выданных номеров партий продуктов переработки зерна \")]"));
+        reestrNomerovLOTPPZButton.click();
+    }
+
+    public void generatePPZnumber() {
+        WebElement putOnDataButton = driver.findElement(By.xpath("//*[contains(text(), \"Внести данные\")]"));
+        putOnDataButton.click();
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+
+        }
+        Select chculturePPZ = new Select(driver.findElement(By.id("input-1353")));
+        ///
     }
 }
--- fgis-navigation/src/test/java/tests/ReestrNomerov.java
+++ fgis-navigation/src/test/java/tests/ReestrNomerov.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ReestrNomerov extends TestBase{
@@ -10,8 +11,29 @@ public class ReestrNomerov extends TestBase{
         app.session().login("Molchanov", "пароль111");
         app.session().goToReestrNomerov();
         app.session().goToReesrrNomerovSDIZ();
+        int lineCount = app.session().getCount();
         app.session().generateNumber();
+        int newlineCount = app.session().getCount();
+        Assertions.assertTrue(app.session().checkTablePart());
+        Assertions.assertEquals(lineCount, newlineCount);
+    }
 
 
+    @Test
+    void generateLotNumber() {
+        app.session().login("Molchanov", "пароль111");
+        app.session().goToReestrNomerov();
+        app.session().goToReestrNomerovLots();
+        app.session().generatelotNumber();
+        Assertions.assertTrue(app.session().checkTablePart());
+    }
+
+
+    @Test
+    void generateLotPPZNumber() {
+        app.session().login("Molchanov", "пароль111");
+        app.session().goToReestrNomerov();
+        app.session().goToReestrNomerovLotsPPZ();
+        Assertions.assertTrue(app.session().checkTablePart());
     }
 }
